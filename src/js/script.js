65b5f377955a0c8463e83d73a601f34f82ccc169
EduardoMoraes <eduzinho2118@gmail.com>
2025-01-16 19:43:19 -0300

    modificação online

--- src/js/script.js
+++ src/js/script.js
@@ -16,18 +16,26 @@ conversor_money()
 
 
 //Ajuda para Pintar a parede da sua casa
-function pintura(){
+function pintura() {
+    const calcular = document.getElementById('calcular')
     const altura = document.getElementById('altura')
     const largura = document.getElementById('largura')
     const resultado = document.getElementById('pintura_res')
     const resultado2 = document.getElementById('resposta2')
 
-    let Altura = parseFloat(altura.value)
-    let Largura = parseFloat(largura.value)
+    calcular.addEventListener('click', () => {
+        let Altura = parseFloat(altura.value)
+        let Largura = parseFloat(largura.value)
 
-    let area = Largura * Altura
+        let area = Largura * Altura
+        let tinta =  area /2
 
-    resultado.innerHTML = `Sua parede tem dimensões de ${Altura}x${Largura} 
-    e tem a area de ${area}`
+        resultado.innerHTML = `Sua parede tem dimensões de ${Altura}x${Largura} 
+        e tem a area de ${area}`
+
+        resultado2.innerHTML = `Para pintar essa parede 
+        voce  precisará de ${tinta.toFixed(2)}l de tinta `
+    })
+
 }
 pintura()
