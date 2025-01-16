127d56991f85dbf5880b781929f7210eb41015d3
EduardoMoraes <eduzinho2118@gmail.com>
2025-01-15 22:10:55 -0300

    testando

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+//converter moeda para dolar
+function conversor_money() {
+    const moeda = document.getElementById('moeda');
+    const converter = document.getElementById('converter');
+    const res_conversao = document.getElementById('resultado_conversao');
+
+    converter.addEventListener('click', () => {
+        let real = parseFloat(moeda.value);
+        let valor_dolar = real / 6.08
+
+        res_conversao.innerHTML = `Seu saldo em reais é R$${real} 
+        em dolar vai ser cerca de US$${valor_dolar.toFixed(2)}`
+    });
+}
+conversor_money()
+
+//pintar casa
